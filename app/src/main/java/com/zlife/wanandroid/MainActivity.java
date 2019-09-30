e178bd675dc4698acef66a60aaae3ed3b5a60806
GisMap <1539138762@qq.com>
2019-09-30 16:45:49 +0800

    First Commit

---
+++ app/src/main/java/com/zlife/wanandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zlife.wanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author zhangxiao
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
